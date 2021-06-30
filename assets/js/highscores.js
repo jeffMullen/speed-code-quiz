3a2a5fd60eddbdf54a6452489d3bd93ef3a83f9c
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-06-30 15:13:56 -0400

    defining variables on secondary JS sheet

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -9,17 +9,15 @@ console.log(highScoresArr);
 scorePage.setAttribute('style', 'display: block');
 
 highScoresArr = JSON.parse(localStorage.getItem('highscores'));
-console.log(highScoresArr);
 
 highScoresArr = JSON.parse(localStorage.getItem('highscores'));
-console.log(highScoresArr.length);
-console.log('highscores array ' + highScoresArr);
 
-
-for (var i = 0; i < highScoresArr.length; i++) {
-    var newLi = document.createElement("li");
-    leaderBoard.appendChild(newLi);
-    newContent = document.createTextNode(highScoresArr[i]);
-    newLi.appendChild(newContent);
+if (highScoresArr !== null) {
+    for (var i = 0; i < highScoresArr.length; i++) {
+        var newLi = document.createElement("li");
+        leaderBoard.appendChild(newLi);
+        newContent = document.createTextNode(highScoresArr[i]);
+        newLi.appendChild(newContent);
+    }
 }
 
